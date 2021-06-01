e54228a63fdb7dd6ee0ed7d43a9f9ecf435e445a
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-06-01 22:00:10 +0530

    commit 3

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const socketio = require('socket.io')
+const io = socketio(http);
+const PORT = process.env.PORT || 5000
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on('create-room', name =>{
+        console.log('Then room name received is', name)
+    })
+});
+
+http.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
