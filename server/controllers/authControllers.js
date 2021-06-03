ed368b7f5a52a902c80b73bc35ba91434c544b94
veena-yadav <59829350+veena-yadav@users.noreply.github.com>
2021-06-03 23:48:54 +0530

    commit 3 day3

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,38 +1,40 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const maxAge = 5 * 24 * 60 * 60
 
-const alertError =(err)=>{
-    let errors = {name:'', email:'', password:''}
-    // console.log(`error meaasge: ${err.message}`)
-    // console.log(`error code: ${err.code}`)
-    // console.log(err);
-    if(err.code === 11000){
-        errors.email= 'This email already exist'
+const createJWT = id => {
+    return jwt.sign({ id }, 'chatroom secret', {
+        expiresIn: maxAge
+    })
+}
+const alertError = (err) => {
+    let errors = { name: '', email: '', password: '' }
+
+    if (err.code === 11000) {
+        errors.email = 'This email already registered';
         return errors;
     }
+    if (err.message.includes('user validation failed')) {
 
-    if(err.message.includes('user validation failed')){
-        console.log(Object.values(err.errors).forEach(({properties}) =>{
-            // console.log(properties)
+        Object.values(err.errors).forEach(({ properties }) => {
             errors[properties.path] = properties.message
-        }))
+        })
     }
     return errors
 }
-
-module.exports.signup = async (req,res)=>{
-    // console.log('req.body',req.body)
-    const {name,email,password} = req.body
+module.exports.signup = async (req, res) => {
+    const { name, email, password } = req.body;
     try {
-       const user = await User.create({name,email,password})
-       res.status(201).json({user})
+        const user = await User.create({ name, email, password });
+        const token = createJWT(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+        res.status(201).json({ user });
     } catch (error) {
-        let errors = alertError(error)
-        // res.status().send('error dyring created a user')
-        res.status(400).json({errors})
+        let errors = alertError(error);
+        res.status(400).json({ errors });
     }
-    
-}
 
+}
 module.exports.login = (req,res)=>{
     res.send('login')
 }
